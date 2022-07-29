e44af16128852bceecfce9f55c1acd2dda95ac47
TorgashinAA <torgashin.aa@yandex.ru>
2022-07-29 16:08:05 +0700

    Added appCard style

---
+++ SideMenu/Models/AppCard.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SideMenu.Models
+{
+    /// <summary>
+    /// Логика взаимодействия для AppCard.xaml
+    /// </summary>
+    public partial class AppCard : UserControl
+    {
+        public AppCard()
+        {
+            InitializeComponent();
+        }
+    }
+}

31c73155e1cc81977f9f367e219aee55f4f0d53c
TorgashinAA <torgashin.aa@yandex.ru>
2022-09-13 11:35:15 +0700

    .

---
+++ SideMenu/Service/Commands/ChangeSideCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SideMenu.Service
+{
+    public class ChangeSideCommand : Command
+    {
+        public ChangeSideCommand()
+        {
+            //TODO: realize change side task bar button
+        }
+    }
+}
